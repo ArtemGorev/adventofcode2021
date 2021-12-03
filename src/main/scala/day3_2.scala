625e508ede82ca5e18bb1f35b3b2f489f4da3caa
Aleksandr Kuznetsov <kuz.mordas@yandex.ru>
2021-12-03 10:55:08 +0300

    feat: finished day 3

--- src/main/scala/day3_2.scala
+++ src/main/scala/day3_2.scala
@@ -1,28 +1,52 @@
 import java.lang.Integer.parseInt
+import scala.annotation.tailrec
 import scala.io.Source
 
 object day3_2 extends App {
 
-  val filename = "inputs/day3_1.txt"
-  val source   = Source.fromFile(filename)
-  val lines    = source.getLines().toList
-  val count: Array[Int] = lines
-    .map(_.split("").map(_.toInt))
-    .reduce((a, b) => sum(a, b))
-  val limit = lines.length / 2
+  val filename    = "inputs/day3_1.txt"
+  val source      = Source.fromFile(filename)
+  val lines       = source.getLines().toList
+  val linesOfInts = lines.map(_.split("").map(_.toInt))
 
-  private val booleans = count.map(a => a > limit)
+//  println(s"Oxygen => ${calcOxygen(linesOfInts)}")
+//  println(s"CO2 => ${calcCO(linesOfInts)}")
+  println(calcOxygen(linesOfInts) * calcCO(linesOfInts))
 
-  val gammaRate = parseInt(booleans
-                             .map(a => if (a) "1" else "0")
-                             .reduce((a, b) => a + b),
-                           2)
+  @tailrec
+  def calcOxygen(l: List[Array[Int]], i: Int = 0): Int = {
+    val bits = calcBits(l)
+    val next =
+      if (bits(i) == 1 || bits(i) == -1)
+        l.filter(x => x(i) == 1)
+      else
+        l.filter(x => x(i) == 0)
 
-  val epsilonRate = parseInt(booleans
-                               .map(a => !a)
-                               .map(a => if (a) "1" else "0")
-                               .reduce((a, b) => a + b),
-                             2)
+    if (next.length == 1)
+      parseInt(next.head.map(_.toString).reduce((a, b) => a + b), 2)
+    else
+      calcOxygen(next, i + 1)
+  }
+
+  @tailrec
+  def calcCO(l: List[Array[Int]], i: Int = 0): Int = {
+    val bits = calcBits(l).map(x => if (x == -1) x else if (x == 1) 0 else 1)
+    val next =
+      if (bits(i) == 1)
+        l.filter(x => x(i) == 1)
+      else
+        l.filter(x => x(i) == 0)
+
+    if (next.length == 1)
+      parseInt(next.head.map(_.toString).reduce((a, b) => a + b), 2)
+    else
+      calcCO(next, i + 1)
+  }
+
+  def calcBits(l: List[Array[Int]]) = {
+    val sumLst = l.reduce((a, b) => sum(a, b))
+    sumLst.map(a => if (l.length - a == a) -1 else if (l.length - a < a) 1 else 0)
+  }
 
   def sum(a: Array[Int], b: Array[Int]): Array[Int] = a.zip(b).map(a => a._1 + a._2)
 }
