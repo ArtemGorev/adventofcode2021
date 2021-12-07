0ee44cd88ab4b60893dd00a956baa72b00ec4089
Aleksandr Kuznetsov <kuz.mordas@yandex.ru>
2021-12-07 08:44:00 +0300

    feat: day 7 part two done

--- src/main/scala/day7_1.scala
+++ src/main/scala/day7_1.scala
@@ -6,12 +6,18 @@ object day7_1 extends App {
   val source   = Source.fromFile(filename)
   val crabs    = source.getLines().toList.head.split(",").map(_.toInt).toList
 
-  def findMinFuel(lst: List[Int]) = {
-    val min = lst.min
-    val max = lst.max
-    (min to max).map(x => lst.map(y => Math.abs(x - y)).sum).min
-  }
+  def findMinFuel(lst: List[Int]) =
+    (lst.min to lst.max).map(x => lst.map(y => Math.abs(x - y)).sum).min
+
+  def findMinFuel2(lst: List[Int]) =
+    (lst.min to lst.max)
+      .map(x => lst.map(y => sum(Math.abs(x - y))).sum)
+      .min
+      .toInt
+
+  def sum(n: Int) = ((1 + n) / 2.0) * n
 
   println(s"part one result: ${findMinFuel(crabs)}")
+  println(s"part two result: ${findMinFuel2(crabs)}")
 
 }
