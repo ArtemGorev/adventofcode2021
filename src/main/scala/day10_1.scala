4c157da1647d8ccaf71b03cd9638748c893042be
Aleksandr Kuznetsov <kuz.mordas@yandex.ru>
2021-12-10 08:54:18 +0300

    feat: day 10 part two done

--- src/main/scala/day10_1.scala
+++ src/main/scala/day10_1.scala
@@ -1,5 +1,3 @@
-import day9_1.source
-
 import scala.collection.mutable
 import scala.io.Source
 
@@ -30,14 +28,14 @@ object day10_1 extends App {
     .toList
     .map(_.split("").toList)
 
-  val result = lines
+  val result1 = lines
     .map(findFirstIllegalChar)
     .filter(_.nonEmpty)
     .groupBy(x => x)
     .map(x => points(x._1) * x._2.length)
     .sum
 
-  println(s"part one result => $result")
+  println(s"part one result => $result1")
 
   def findFirstIllegalChar(line: List[String]) = {
     val stack       = mutable.Stack[String]()
@@ -58,4 +56,50 @@ object day10_1 extends App {
     illegalChar
   }
 
+  val result2 = lines
+    .filter(l => findFirstIllegalChar(l).isEmpty)
+    .map(completeString)
+    .map(calcTotalScore)
+    .sorted
+
+  println(s"part two result => ${result2(result2.length / 2)}")
+
+  def completeString(line: List[String]) = {
+    val stack = mutable.Stack[String]()
+    var chars = line
+
+    while (chars.nonEmpty) {
+      val char = chars.head
+      chars = chars.tail
+      if (openBrackets.contains(char)) {
+        stack.push(char)
+      } else {
+        stack.pop()
+      }
+    }
+
+    var result       = List[String]()
+    val inversePairs = pairs.map(x => x.swap)
+
+    while (stack.nonEmpty) {
+      val char = stack.pop()
+      result = result :+ inversePairs(char)
+    }
+
+    result
+  }
+
+  def calcTotalScore(lst: List[String]): Long = {
+    val points = Map(
+      ")" -> 1,
+      "]" -> 2,
+      "}" -> 3,
+      ">" -> 4
+    )
+
+    lst.foldLeft(0.toLong)((acc, curr) => {
+      val point = points(curr)
+      acc * 5 + point
+    })
+  }
 }
