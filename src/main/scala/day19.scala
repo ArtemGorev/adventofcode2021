fcb7cd3a0ac5f82b5752dd7cf1a87d3f6d87d95f
Artem Gorev <artgorev@gmail.com>
2021-12-19 17:10:10 +0300

    feat: day 19 complete

--- src/main/scala/day19.scala
+++ src/main/scala/day19.scala
@@ -1,3 +1,4 @@
+import scala.annotation.tailrec
 import scala.io.Source
 
 trait day19_fn {
@@ -10,58 +11,115 @@ trait day19_fn {
     seq
   }
 
-  def rotate(s: Scan, rotate: Rotation): Scan = {
-    val rotated = rotate match {
-      case Rotation(0, 0, 0, _) => s
-      case Rotation(1, 0, 0, _) => s.copy(x = -s.x)
-      case Rotation(0, 1, 0, _) => s.copy(y = -s.y)
-      case Rotation(0, 0, 1, _) => s.copy(z = -s.z)
-      case Rotation(1, 1, 0, _) => s.copy(x = -s.x, y = -s.y)
-      case Rotation(0, 1, 1, _) => s.copy(y = -s.y, z = -s.z)
-      case Rotation(1, 0, 1, _) => s.copy(x = -s.x, z = -s.z)
-      case Rotation(1, 1, 1, _) => s.copy(x = -s.x, y = -s.y, z = -s.z)
+  type Scanner = Set[Point]
+
+  def parseScanners(lines: Seq[String]): Seq[Scanner] = lines.mkString("\n").split("\n\n").map(parseScanner)
+
+  def parseScanner(s: String): Scanner = s.linesIterator
+    .drop(1)
+    .map(line => {
+      val Seq(x, y, z) = line.split(",").toSeq
+      Point(x.toInt, y.toInt, z.toInt)
+    }).toSet
+
+  def getAmountOfBeacons(scanners: Seq[Scanner]): Int = createMap(scanners)._1.size
+
+  def createMap(scanners: Seq[Scanner]): (Scanner, Set[Point]) = {
+
+    @tailrec
+    def helper(scanners: Seq[Scanner], beacons: Scanner, next: Scanner, scannerPoints: Set[Point]): (Scanner, Set[Point]) = {
+      val newBeacons = beacons ++ next
+      if (scanners.isEmpty)
+        (newBeacons, scannerPoints)
+      else {
+        val (matchedScanners, orientedScanners, matchedScannerPoss) = (for {
+          scanner <- scanners
+          (orientedScanner, scannerPos) <- matchScanner(next, scanner)
+        } yield (scanner, orientedScanner, scannerPos)).unzip3
+        val newScanners = scanners.filterNot(matchedScanners.contains)
+        val nextScanner = orientedScanners.reduce(_ ++ _)
+        val newScannerPoints = scannerPoints ++ matchedScannerPoss
+        helper(newScanners, newBeacons, nextScanner, newScannerPoints)
+      }
     }
 
-    val oriented = rotate match {
-      case Rotation(_,_,_, Up) => s.copy()
-      case Rotation(_,_,_, Down) => s.copy()
-      case Rotation(_,_,_, Left) => s.copy()
-      case Rotation(_,_,_, Right) => s.copy()
-    }
+    val scanner0 +: scannersTail = scanners
+    helper(scannersTail, Set.empty, scanner0, Set(Point(0, 0, 0))) // start with scanner 0 fixed
   }
 
-  def order(): List[Rotation] = {
-    List[Rotation](
-      Rotation(1, 0, 0),
-      Rotation(0, 1, 0),
-      Rotation(0, 0, 1),
-      Rotation(1, 1, 0),
-      Rotation(0, 1, 1),
-      Rotation(1, 0, 1),
-      Rotation(1, 1, 1)
+  def matchScanner(beacons: Scanner, scanner: Scanner): Option[(Scanner, Point)] =
+    (for {
+      orientedScanner <- scannerOrientations(scanner).toList
+      ds1 = for {
+        p1: Point <- beacons.toList
+        p2: Point <- orientedScanner.toList
+      } yield {
+        p1 - p2
+      }
+      ds = groupCount(ds1)
+      (d, cnt) <- ds if cnt >= 12
+    } yield (orientedScanner.map(_ + d), d)).headOption
+
+  def groupCount(ds: Seq[Point]) = ds.toList.distinct.map(d => (d, ds.count(_ == d)))
+
+  def scannerOrientations(scanner: Scanner): Seq[Scanner] = scanner.toSeq.map(orientate).transpose.map(a => a.toSet)
+
+  def orientate(point: Point): Seq[Point] = {
+    val Point(x, y, z) = point
+    Seq(
+      Point(x, y, z),
+      Point(x, -y, -z),
+      Point(x, -z, y),
+      Point(x, z, -y),
+      Point(-x, z, y),
+      Point(-x, -y, z),
+      Point(-x, y, -z),
+      Point(-x, -z, -y),
+      Point(-y, x, z),
+      Point(y, -x, z),
+      Point(y, x, -z),
+      Point(-y, -x, -z),
+      Point(-z, y, x),
+      Point(-z, x, -y),
+      Point(-z, -y, -x),
+      Point(-z, -x, y),
+      Point(z, y, -x),
+      Point(z, x, y),
+      Point(z, -y, x),
+      Point(z, -x, -y),
+      Point(-y, -z, x),
+      Point(y, -z, -x),
+      Point(-y, z, -x),
+      Point(y, z, x),
     )
   }
 
-  case class Scan(x: Int, y: Int, z: Int)
-  case class Scanner(scans: Seq[Scan])
-  case class Rotation(x: Int, y: Int, z: Int, orentation: Orientation)
+  def largestDistance(scanners: Seq[Scanner]): Int = {
+    val (_, scannerPoss) = createMap(scanners)
+    (for {
+      p1 <- scannerPoss
+      p2 <- scannerPoss
+    } yield p1 manhattanDistance p2).max
+  }
 
-  sealed class Orientation(degree: Int)
-  object Up extends Orientation(0)
-  object Down extends Orientation(180)
-  object Left extends Orientation(270)
-  object Right extends Orientation(90)
+  sealed trait Orientation
+
+  case class Point(x: Int, y: Int, z: Int) {
+    def manhattanDistance(that: Point): Int =
+      (x - that.x).abs + (y - that.y).abs + (z - that.z).abs
+
+    def -(that: Point): Point =
+      Point(x - that.x, y - that.y, z - that.z)
+
+    def +(that: Point): Point =
+      Point(x + that.x, y + that.y, z + that.z)
+  }
 }
 
 object day19 extends App with day19_fn {
-  val filename = "inputs/day18_1.txt"
+  val filename = "inputs/day19_1.txt"
+  val scanners = parseScanners(readFile(filename))
 
-
-  val origin = Scan(-1, -1, 1)
-  val scan = order().foldLeft[Scan](origin)((a: Scan, b: Rotation) => {
-    val rotatedScan = rotate(a, b)
-    println(s"rotatedScan => ${rotatedScan}")
-    rotatedScan
-  })
-
+  println(getAmountOfBeacons(scanners))
+  println(largestDistance(scanners))
 }
